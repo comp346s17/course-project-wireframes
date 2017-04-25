3e34d1dedff79fb6ab1d0e3eb5d2ca61a1712efa
Vihan Agarwal <vihanagarwal97@gmail.com>
2017-04-25 18:41:52 -0500

    models added

--- linkbucket/main/models.py
+++ linkbucket/main/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+#Model for a single bucket
+class Bucket(models.Model):
+	title = models.TextField()
+	creator = models.ForeignKey('auth.user')
+	links = models.OneToManyField('Link')
+
+
+#Model for single link
+class Link(models.Model):
+	title = models.TextField()
+	bucket = models.OneToManyField('Bucket')
+	link_ref = models.TextField()
+
+

193ed8b9e2d77223c84fb8c08eb9088a5e06a51c
Eric <ehuang@macalester.edu>
2017-05-03 08:28:45 -0500

    FInished auth models

--- linkbucket/main/models.py
+++ linkbucket/main/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -34,3 +36,32 @@ class Link(models.Model):
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
